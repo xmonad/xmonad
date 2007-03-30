75187c4b4113d801afbd73abdc225412c0b6e0bb
hughes <hughes@rpi.edu>
2007-03-30 14:45:58 +0000

    Remove evil gmrun shortcut.

--- Config.hs
+++ Config.hs
@@ -70,7 +70,7 @@ keys :: M.Map (KeyMask, KeySym) (X ())
 keys = M.fromList $
     [ ((modMask .|. shiftMask, xK_Return), spawn "xterm")
     , ((modMask,               xK_p     ), spawn "exe=`dmenu_path | dmenu` && exec $exe")
-    , ((controlMask,           xK_space ), spawn "gmrun")
+--    , ((modMask .|. shiftMask, xK_F11   ), spawn "gmrun")
     , ((modMask,               xK_Tab   ), raise GT)
     , ((modMask,               xK_j     ), raise GT)
     , ((modMask,               xK_k     ), raise LT)
