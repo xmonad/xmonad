d904fb1cc4296e411f3cba945a5f4dac81a4b08d
Adam Vogt <vogt.adam@gmail.com>
2012-12-31 16:34:03 +0000

    Update generated manpage (and releaseDate in util/GenerateManpage.hs)

--- util/GenerateManpage.hs
+++ util/GenerateManpage.hs
@@ -36,7 +36,7 @@ import Distribution.Text
 
 import Text.Pandoc -- works with 1.6
 
-releaseDate = "18 November 2011"
+releaseDate = "31 December 2012"
 
 trim :: String -> String
 trim = reverse . dropWhile isSpace . reverse . dropWhile isSpace
