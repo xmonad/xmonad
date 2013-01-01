d386a230f6cf025795d6114b6570821ecf28979e
Adam Vogt <vogt.adam@gmail.com>
2013-01-01 03:50:34 +0000

    Add flags for call to ghc closing issue 240
    The -main-is flag goes back to at least ghc 6.10, and maybe the warning that
    this otherwise redundant flag enables (when xmonad.hs isn't a  module Main)
    also dates back that far.

--- XMonad/Core.hs
+++ XMonad/Core.hs
@@ -460,7 +460,7 @@ recompile force = io $ do
         -- temporarily disable SIGCHLD ignoring:
         uninstallSignalHandlers
         status <- bracket (openFile err WriteMode) hClose $ \h ->
-            waitForProcess =<< runProcess "ghc" ["--make", "xmonad.hs", "-i", "-ilib", "-fforce-recomp", "-v0", "-o",binn] (Just dir)
+            waitForProcess =<< runProcess "ghc" ["--make", "xmonad.hs", "-i", "-ilib", "-fforce-recomp", "-main-is", "main", "-v0", "-o",binn] (Just dir)
                                     Nothing Nothing Nothing (Just h)
 
         -- re-enable SIGCHLD:
