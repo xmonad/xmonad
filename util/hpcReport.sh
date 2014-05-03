ecde376224d024c3c54f197be4a79e5ef7be0bac
Adam Vogt <vogt.adam@gmail.com>
2014-05-03 03:41:25 +0000

    make hpc report cover the testsuite too

--- util/hpcReport.sh
+++ util/hpcReport.sh
@@ -16,13 +16,16 @@ propsExclude=$(find tests/Properties -name '*.hs' \
         | sed -e 's_/_._g' -e 's_.hs$__' -e 's_^tests._--exclude=_' )
 
 hpcFlags="
-  --exclude=Instances
-  --exclude=Utils
-  --exclude=Main
-  $propsExclude
-  --hpcdir=dist/hpc/mix/xmonad-0.12/
+  --hpcdir=dist/hpc/mix/
   dist/hpc/tix/properties/properties.tix
   "
+
+
+if [[ ! (-e dist/hpc/mix/Main.mix) ]]; then
+  mv dist/hpc/mix/properties/* dist/hpc/mix/
+  mv dist/hpc/mix/xmonad-*/xmonad-*/* dist/hpc/mix/xmonad-*/
+fi
+
 
 hpc markup --destdir=dist/hpc $hpcFlags > /dev/null
 echo "see dist/hpc/hpc_index.html
