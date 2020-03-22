096d2c758ef3e95f213965676a8ab9e447325504
Eli Peery <eli.g.peery@gmail.com>
2020-03-22 14:24:37 -0400

    Pinned nixpkgs version

---
+++ release.nix
@@ -0,0 +1,26 @@
+let
+  config = {
+    packageOverrides = pkgs: rec {
+      haskellPackages = pkgs.haskellPackages.override {
+        overrides = haskellPackagesNew: haskellPackagesOld: rec {
+          app =
+            haskellPackagesNew.callPackage ./default.nix { };
+        };
+      };
+    };
+  };
+
+  bootstrap = import <nixpkgs> { };
+
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+
+  pkgs = import src { inherit config; };
+
+in
+  { app = pkgs.haskellPackages.app; pkgs = pkgs;}
