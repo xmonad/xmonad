xmonad/xmonad

commit 30719202b9daa80fe1ab37293a4615b93cb042d9

Fix xmessage in non-UTF-8 locales
`executeFile` encodes the arguments with the current locale's encoding,
and GHC as invoked during recompilation quite likely also outputs any
errors in the locale encoding, which we then read using `readFile` again
decoding in the locale encoding, so it really makes no sense to force a
specific encoding here. What was I thinking? :-)

Related: https://github.com/xmonad/xmonad/issues/322#issuecomment-900503386
Fixes: https://github.com/xmonad/xmonad/issues/324
Fixes: aa35ea185683 ("Make xmessage handle UTF-8 and export it")

src/XMonad/Core.hs
@@ -459,7 +459,6 @@ xfork x = io . forkProcess . finally nullStdin $ do
 -- | Use @xmessage@ to show information to the user.
 xmessage :: MonadIO m => String -> m ()
 xmessage msg = void . xfork $ do
-    setEnv "LC_ALL" "C.UTF-8"
     executeFile "xmessage" True
         [ "-default", "okay"
         , "-xrm", "*international:true"

commit f89df98f4022b5142760b86a2ad85b4e90e583b3

Drop unused import System.Environment (setEnv)
Fixes: 30719202b9da ("Fix xmessage in non-UTF-8 locales")

src/XMonad/Core.hs
@@ -56,7 +56,6 @@ import System.Posix.IO
 import System.Posix.Types (ProcessID)
 import System.Process
 import System.Directory
-import System.Environment (setEnv)
 import System.Exit
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras (getWindowAttributes, WindowAttributes, Event)